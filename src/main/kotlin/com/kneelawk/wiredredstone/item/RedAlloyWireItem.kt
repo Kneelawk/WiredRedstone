10123508fb5683d9b180dbf4b7c386e530a22042
Kneelawk <kneelawk@gmail.com>
2022-01-07 07:26:59 -0800

    Improve red alloy wire placement again

--- src/main/kotlin/com/kneelawk/wiredredstone/item/RedAlloyWireItem.kt
+++ src/main/kotlin/com/kneelawk/wiredredstone/item/RedAlloyWireItem.kt
@@ -1,20 +1,14 @@
 package com.kneelawk.wiredredstone.item
 
 import alexiil.mc.lib.multipart.api.MultipartHolder
-import alexiil.mc.lib.multipart.api.MultipartUtil
 import com.kneelawk.wiredredstone.part.RedAlloyWirePart
 import com.kneelawk.wiredredstone.part.WRParts
-import com.kneelawk.wiredredstone.util.WireUtils.isValidFace
+import com.kneelawk.wiredredstone.util.PlacementUtils
+import net.minecraft.block.Blocks
 import net.minecraft.item.Item
 import net.minecraft.item.ItemUsageContext
 import net.minecraft.util.ActionResult
-import net.minecraft.util.math.BlockPos
 import net.minecraft.util.math.Direction
-import net.minecraft.util.math.Vec3d
-import net.minecraft.world.BlockView
-import kotlin.math.abs
-import kotlin.math.ceil
-import kotlin.math.floor
 
 class RedAlloyWireItem(settings: Settings) : Item(settings) {
     override fun useOnBlock(context: ItemUsageContext): ActionResult {
@@ -23,80 +17,11 @@ class RedAlloyWireItem(settings: Settings) : Item(settings) {
             return ActionResult.PASS
         }
 
-        val pos = context.blockPos
-        val side = context.side
-        val hitPos = context.hitPos.subtract(Vec3d.ofCenter(pos))
-        val offsetPos = pos.offset(side)
-        val state = world.getBlockState(pos)
-        val sideSide = closestSideSide(hitPos, side)
+        val offer = PlacementUtils.tryPlace(context, ::creator) ?: return ActionResult.FAIL
 
-        val offer = if (isOnExternalSide(hitPos, side)) {
-            if (isValidFace(state, world, pos, side)) {
-                MultipartUtil.offerNewPart(world, offsetPos, creator(side.opposite)) ?: return ActionResult.FAIL
-            } else {
-                val sidePos = offsetPos.offset(sideSide)
-                val sideState = world.getBlockState(sidePos)
-                if (isValidFace(sideState, world, sidePos, sideSide.opposite)) {
-                    MultipartUtil.offerNewPart(world, offsetPos, creator(sideSide)) ?: return ActionResult.FAIL
-                } else {
-                    findValidFace(world, offsetPos)?.let {
-                        MultipartUtil.offerNewPart(world, offsetPos, creator(it))
-                    } ?: return ActionResult.FAIL
-                }
-            }
-        } else {
-            val offsetState = world.getBlockState(offsetPos)
-            if (isValidFace(offsetState, world, offsetPos, side.opposite)) {
-                MultipartUtil.offerNewPart(world, pos, creator(side)) ?: return ActionResult.FAIL
-            } else {
-                val sidePos1 = pos.offset(sideSide)
-                val sideState1 = world.getBlockState(sidePos1)
-                (if (isValidFace(sideState1, world, sidePos1, sideSide.opposite)) {
-                    MultipartUtil.offerNewPart(world, pos, creator(sideSide))
-                } else null) ?: run {
-                    val sidePos2 = offsetPos.offset(sideSide)
-                    val sideState2 = world.getBlockState(sidePos2)
-                    if (isValidFace(sideState2, world, sidePos2, sideSide.opposite)) {
-                        MultipartUtil.offerNewPart(world, offsetPos, creator(sideSide)) ?: return ActionResult.FAIL
-                    } else {
-                        findValidFace(world, offsetPos)?.let {
-                            MultipartUtil.offerNewPart(world, offsetPos, creator(it))
-                        } ?: return ActionResult.FAIL
-                    }
-                }
-            }
-        }
-
-        offer.apply()
-        offer.holder.part.onPlacedBy(context.player, context.hand)
-        context.stack.decrement(1)
+        PlacementUtils.finishPlacement(context, offer, Blocks.REDSTONE_BLOCK.defaultState)
 
         return ActionResult.SUCCESS
-    }
-
-    private fun closestSideSide(hitPos: Vec3d, hitSide: Direction): Direction {
-        val zeroed = hitPos.withAxis(hitSide.axis, 0.0)
-        return Direction.getFacing(zeroed.x, zeroed.y, zeroed.z)
-    }
-
-    private fun isOnExternalSide(hitPos: Vec3d, hitSide: Direction): Boolean {
-        val d = hitPos.getComponentAlongAxis(hitSide.axis) + 0.5
-        return almostEquals(d, floor(d)) || almostEquals(d, ceil(d))
-    }
-
-    private fun almostEquals(d1: Double, d2: Double): Boolean {
-        return abs(d1 - d2) < 0.001
-    }
-
-    private fun findValidFace(world: BlockView, pos: BlockPos): Direction? {
-        for (side in Direction.values()) {
-            val sidePos = pos.offset(side)
-            val sideState = world.getBlockState(sidePos)
-            if (isValidFace(sideState, world, sidePos, side.opposite)) {
-                return side
-            }
-        }
-        return null
     }
 
     private fun creator(side: Direction): ((MultipartHolder) -> RedAlloyWirePart) {
---
+++ src/main/kotlin/com/kneelawk/wiredredstone/util/PlacementUtils.kt
@@ -0,0 +1,118 @@
+package com.kneelawk.wiredredstone.util
+
+import alexiil.mc.lib.multipart.api.MultipartContainer
+import alexiil.mc.lib.multipart.api.MultipartHolder
+import alexiil.mc.lib.multipart.api.MultipartUtil
+import com.kneelawk.wiredredstone.part.RedAlloyWirePart
+import net.minecraft.block.BlockState
+import net.minecraft.item.ItemUsageContext
+import net.minecraft.sound.SoundCategory
+import net.minecraft.sound.SoundEvent
+import net.minecraft.util.math.BlockPos
+import net.minecraft.util.math.Direction
+import net.minecraft.util.math.Vec3d
+import net.minecraft.world.BlockView
+import net.minecraft.world.World
+import kotlin.math.abs
+
+object PlacementUtils {
+    fun finishPlacement(
+        context: ItemUsageContext, offer: MultipartContainer.PartOffer? = null, closestBlockState: BlockState
+    ) {
+        val soundGroup = closestBlockState.soundGroup
+        finishPlacement(context, offer, soundGroup.placeSound, soundGroup.volume, soundGroup.pitch)
+    }
+
+    fun finishPlacement(
+        context: ItemUsageContext, offer: MultipartContainer.PartOffer? = null, placeSound: SoundEvent, volume: Float,
+        pitch: Float
+    ) {
+        val player = context.player
+        if (player == null || !player.abilities.creativeMode) {
+            context.stack.decrement(1)
+        }
+
+        context.world.playSound(
+            null,
+            context.blockPos,
+            placeSound,
+            SoundCategory.BLOCKS,
+            (volume + 1f) / 2f,
+            pitch * 0.8f
+        )
+
+        offer?.let {
+            it.apply()
+            it.holder.part.onPlacedBy(player, context.hand)
+        }
+    }
+
+    fun tryPlace(
+        context: ItemUsageContext, creatorFactory: (Direction) -> ((MultipartHolder) -> RedAlloyWirePart)
+    ): MultipartContainer.PartOffer? {
+        val world = context.world
+        val pos = context.blockPos
+        val side = context.side
+        val hitPos = context.hitPos.subtract(Vec3d.ofCenter(pos))
+        val offsetPos = pos.offset(side)
+        val state = world.getBlockState(pos)
+        val sideSide = closestSideSide(hitPos, side)
+
+        return if (isOnExternalSide(hitPos, side)) {
+            (if (WireUtils.isValidFace(state, world, pos, side)) {
+                MultipartUtil.offerNewPart(world, offsetPos, creatorFactory(side.opposite))
+            } else null) ?: tryOffsetSide(offsetPos, sideSide, world, creatorFactory)
+        } else {
+            val offsetState = world.getBlockState(offsetPos)
+            if (WireUtils.isValidFace(offsetState, world, offsetPos, side.opposite)) {
+                MultipartUtil.offerNewPart(world, pos, creatorFactory(side))
+            } else {
+                val sidePos1 = pos.offset(sideSide)
+                val sideState1 = world.getBlockState(sidePos1)
+                (if (WireUtils.isValidFace(sideState1, world, sidePos1, sideSide.opposite)) {
+                    MultipartUtil.offerNewPart(world, pos, creatorFactory(sideSide))
+                } else null) ?: tryOffsetSide(offsetPos, sideSide, world, creatorFactory)
+            }
+        }
+    }
+
+    private fun tryOffsetSide(
+        offsetPos: BlockPos, sideSide: Direction,
+        world: World, creatorFactory: (Direction) -> ((MultipartHolder) -> RedAlloyWirePart)
+    ): MultipartContainer.PartOffer? {
+        val sidePos = offsetPos.offset(sideSide)
+        val sideState = world.getBlockState(sidePos)
+        return if (WireUtils.isValidFace(sideState, world, sidePos, sideSide.opposite)) {
+            MultipartUtil.offerNewPart(world, offsetPos, creatorFactory(sideSide))
+        } else {
+            findValidFace(world, offsetPos)?.let {
+                MultipartUtil.offerNewPart(world, offsetPos, creatorFactory(it))
+            }
+        }
+    }
+
+    private fun closestSideSide(hitPos: Vec3d, hitSide: Direction): Direction {
+        val zeroed = hitPos.withAxis(hitSide.axis, 0.0)
+        return Direction.getFacing(zeroed.x, zeroed.y, zeroed.z)
+    }
+
+    private fun isOnExternalSide(hitPos: Vec3d, hitSide: Direction): Boolean {
+        val d = hitPos.getComponentAlongAxis(hitSide.axis)
+        return almostEquals(d, -0.5) || almostEquals(d, 0.5)
+    }
+
+    private fun almostEquals(d1: Double, d2: Double): Boolean {
+        return abs(d1 - d2) < 0.001
+    }
+
+    private fun findValidFace(world: BlockView, pos: BlockPos): Direction? {
+        for (side in Direction.values()) {
+            val sidePos = pos.offset(side)
+            val sideState = world.getBlockState(sidePos)
+            if (WireUtils.isValidFace(sideState, world, sidePos, side.opposite)) {
+                return side
+            }
+        }
+        return null
+    }
+}
