012ec5695eed6fce832719ff41b3eb643e8aaaab
Kneelawk <jedidiah.pommert@gmail.com>
2022-04-15 20:00:45 -0700

    Wire state changes no longer cause wire connection updates

--- src/main/kotlin/com/kneelawk/wiredredstone/util/WorldUtils.kt
+++ src/main/kotlin/com/kneelawk/wiredredstone/util/WorldUtils.kt
@@ -1,5 +1,8 @@
 package com.kneelawk.wiredredstone.util
 
+import alexiil.mc.lib.multipart.api.MultipartUtil
+import com.kneelawk.wiredredstone.wirenet.NetNodeContainer
+import net.minecraft.block.Block
 import net.minecraft.util.math.BlockPos
 import net.minecraft.util.math.Direction
 import net.minecraft.world.World
@@ -9,12 +12,33 @@ object WorldUtils {
         val state = world.getBlockState(pos)
         val offset = pos.offset(edge)
 
-        world.updateNeighbors(pos, state.block)
+        updateNeighbors(world, pos, state.block)
 
         Direction.values()
             .asSequence()
             .filter { it != edge.opposite }
             .map { offset.offset(it) }
-            .forEach { world.updateNeighbor(it, state.block, pos) }
+            .forEach { updateNeighbor(world, it, state.block, pos) }
+    }
+
+    fun updateNeighbors(world: World, sourcePos: BlockPos, sourceBlock: Block) {
+        updateNeighbor(world, sourcePos.west(), sourceBlock, sourcePos)
+        updateNeighbor(world, sourcePos.east(), sourceBlock, sourcePos)
+        updateNeighbor(world, sourcePos.down(), sourceBlock, sourcePos)
+        updateNeighbor(world, sourcePos.up(), sourceBlock, sourcePos)
+        updateNeighbor(world, sourcePos.north(), sourceBlock, sourcePos)
+        updateNeighbor(world, sourcePos.south(), sourceBlock, sourcePos)
+    }
+
+    fun updateNeighbor(world: World, pos: BlockPos, sourceBlock: Block, neighbosPos: BlockPos) {
+        val multipart = MultipartUtil.get(world, pos)
+
+        if (multipart != null) {
+            if (multipart.getFirstPart { it !is NetNodeContainer } != null) {
+                world.updateNeighbor(pos, sourceBlock, neighbosPos)
+            }
+        } else {
+            world.updateNeighbor(pos, sourceBlock, neighbosPos)
+        }
     }
 }
