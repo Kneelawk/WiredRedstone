23a8383627a3c753ad9d9ac72e2ea1db098f2354
Kneelawk <kneelawk@gmail.com>
2023-09-08 20:29:17 -0700

    Add standing wire recipes & polish

--- src/main/kotlin/com/kneelawk/wiredredstone/datagen/WRRecipeGen.kt
+++ src/main/kotlin/com/kneelawk/wiredredstone/datagen/WRRecipeGen.kt
@@ -7,6 +7,8 @@ import com.kneelawk.wiredredstone.util.DyeColorUtil
 import net.fabricmc.fabric.api.datagen.v1.FabricDataOutput
 import net.fabricmc.fabric.api.datagen.v1.provider.FabricRecipeProvider
 import net.minecraft.data.server.recipe.RecipeJsonProvider
+import net.minecraft.item.Item
+import net.minecraft.item.ItemConvertible
 import net.minecraft.item.Items
 import net.minecraft.recipe.RecipeCategory
 import net.minecraft.registry.Registries
@@ -17,13 +19,31 @@ import net.minecraft.util.Identifier
 import java.util.function.Consumer
 
 class WRRecipeGen(output: FabricDataOutput) : FabricRecipeProvider(output) {
-    override fun generateRecipes(exporter: Consumer<RecipeJsonProvider>?) {
+    override fun generateRecipes(exporter: Consumer<RecipeJsonProvider>) {
         val plainBundledCable = WRItems.BUNDLED_CABLES[null]!!
+        val plainStandingBundledCable = WRItems.STANDING_BUNDLED_CABLES[null]!!
+
+        standingConversion(exporter, "standing_red_alloy_wire", WRItems.STANDING_RED_ALLOY_WIRE, WRItems.RED_ALLOY_WIRE)
+        standingConversion(exporter, "standing_bundled_cable", plainStandingBundledCable, plainBundledCable)
+
+        ShapedRARecipeJsonFactory.create(RecipeCategory.REDSTONE, plainStandingBundledCable)
+            .cookTime(50)
+            .ingredient('W', WRTagGen.STANDING_INSULATED_WIRES)
+            .ingredient('S', TagKey.of(RegistryKeys.ITEM, Identifier("c", "string")))
+            .pattern("SWS")
+            .pattern("WWW")
+            .pattern("SWS")
+            .group("standing_bundled_cable")
+            .criterion(hasItem(WRItems.REDSTONE_ALLOY_INGOT), conditionsFromItem(WRItems.REDSTONE_ALLOY_INGOT))
+            .offerTo(exporter)
+
         for (color in DyeColor.values()) {
             val dyeTag = WRTagGen.DYE_TAGS[color]!!
             val bundledCable = WRItems.BUNDLED_CABLES[color]!!
             val insulatedWire = WRItems.INSULATED_WIRES[color]!!
             val insulatedWireId = Registries.ITEM.getId(insulatedWire)
+            val standingBundledCable = WRItems.STANDING_BUNDLED_CABLES[color]!!
+            val standingInsulatedWire = WRItems.STANDING_INSULATED_WIRES[color]!!
 
             ShapelessRARecipeJsonFactory.create(RecipeCategory.REDSTONE, bundledCable)
                 .cookTime(20)
@@ -34,12 +54,7 @@ class WRRecipeGen(output: FabricDataOutput) : FabricRecipeProvider(output) {
                 .criterion(hasItem(plainBundledCable), conditionsFromItem(plainBundledCable))
                 .offerTo(exporter)
 
-            ShapelessRARecipeJsonFactory.create(RecipeCategory.REDSTONE, bundledCable)
-                .ingredient(WRTagGen.COLORED_BUNDLED_CABLES)
-                .ingredient(dyeTag)
-                .group("bundled_cable")
-                .criterion(hasItem(plainBundledCable), conditionsFromItem(plainBundledCable))
-                .offerTo(exporter, Registries.ITEM.getId(bundledCable).extendPath("_recolor"))
+            recolor(exporter, "bundled_cable", bundledCable, dyeTag, WRTagGen.COLORED_BUNDLED_CABLES, plainBundledCable)
 
             ShapedRARecipeJsonFactory.create(RecipeCategory.REDSTONE, insulatedWire, 12)
                 .cookTime(100)
@@ -52,12 +67,10 @@ class WRRecipeGen(output: FabricDataOutput) : FabricRecipeProvider(output) {
                 .criterion(hasItem(WRItems.REDSTONE_ALLOY_INGOT), conditionsFromItem(WRItems.REDSTONE_ALLOY_INGOT))
                 .offerTo(exporter)
 
-            ShapelessRARecipeJsonFactory.create(RecipeCategory.REDSTONE, insulatedWire)
-                .ingredient(WRTagGen.INSULATED_WIRES)
-                .ingredient(dyeTag)
-                .group("insulated_wire")
-                .criterion(hasItem(WRItems.REDSTONE_ALLOY_INGOT), conditionsFromItem(WRItems.REDSTONE_ALLOY_INGOT))
-                .offerTo(exporter, insulatedWireId.extendPath("_recolor"))
+            recolor(
+                exporter, "insulated_wire", insulatedWire, dyeTag, WRTagGen.INSULATED_WIRES,
+                WRItems.REDSTONE_ALLOY_INGOT
+            )
 
             ShapelessRARecipeJsonFactory.create(RecipeCategory.REDSTONE, insulatedWire, 2)
                 .ingredient(WRItems.RED_ALLOY_WIRE)
@@ -66,6 +79,54 @@ class WRRecipeGen(output: FabricDataOutput) : FabricRecipeProvider(output) {
                 .group("insulated_wire")
                 .criterion(hasItem(WRItems.RED_ALLOY_WIRE), conditionsFromItem(WRItems.RED_ALLOY_WIRE))
                 .offerTo(exporter, insulatedWireId.extendPath("_wrapping"))
+
+            standingConversion(exporter, "standing_bundled_cable", standingBundledCable, bundledCable)
+            standingConversion(exporter, "standing_insulated_wire", standingInsulatedWire, insulatedWire)
+
+            recolor(
+                exporter, "standing_bundled_cable", standingBundledCable, dyeTag,
+                WRTagGen.COLORED_STANDING_BUNDLED_CABLES, plainBundledCable
+            )
+            recolor(
+                exporter, "standing_insulated_wire", standingInsulatedWire, dyeTag, WRTagGen.STANDING_INSULATED_WIRES,
+                WRItems.REDSTONE_ALLOY_INGOT
+            )
+
+            ShapelessRARecipeJsonFactory.create(RecipeCategory.REDSTONE, standingBundledCable)
+                .cookTime(20)
+                .ingredient(plainStandingBundledCable)
+                .ingredient(Items.PAPER)
+                .ingredient(dyeTag)
+                .group("standing_bundled_cable")
+                .criterion(hasItem(plainStandingBundledCable), conditionsFromItem(plainStandingBundledCable))
+                .offerTo(exporter)
         }
+    }
+
+    fun standingConversion(
+        exporter: Consumer<RecipeJsonProvider>, group: String, output: ItemConvertible, input: ItemConvertible
+    ) {
+        ShapedRARecipeJsonFactory.create(RecipeCategory.REDSTONE, output, 4)
+            .cookTime(40)
+            .ingredient('W', input)
+            .ingredient('I', TagKey.of(RegistryKeys.ITEM, Identifier("c", "iron_ingots")))
+            .pattern(" W ")
+            .pattern("WIW")
+            .pattern(" W ")
+            .group(group)
+            .criterion(hasItem(input), conditionsFromItem(input))
+            .offerTo(exporter, Registries.ITEM.getId(output.asItem()).extendPath("_standing_conversion"))
+    }
+
+    fun recolor(
+        exporter: Consumer<RecipeJsonProvider>, group: String, output: ItemConvertible, dyeTag: TagKey<Item>,
+        inputTag: TagKey<Item>, criterion: ItemConvertible
+    ) {
+        ShapelessRARecipeJsonFactory.create(RecipeCategory.REDSTONE, output)
+            .ingredient(inputTag)
+            .ingredient(dyeTag)
+            .group(group)
+            .criterion(hasItem(criterion), conditionsFromItem(criterion))
+            .offerTo(exporter, Registries.ITEM.getId(output.asItem()).extendPath("_recolor"))
     }
 }
--- src/main/kotlin/com/kneelawk/wiredredstone/datagen/WRTagGen.kt
+++ src/main/kotlin/com/kneelawk/wiredredstone/datagen/WRTagGen.kt
@@ -5,6 +5,7 @@ import com.kneelawk.wiredredstone.item.WRItems
 import com.kneelawk.wiredredstone.util.DyeColorUtil
 import net.fabricmc.fabric.api.datagen.v1.FabricDataOutput
 import net.fabricmc.fabric.api.datagen.v1.provider.FabricTagProvider.ItemTagProvider
+import net.minecraft.item.Item
 import net.minecraft.registry.HolderLookup
 import net.minecraft.registry.RegistryKeys
 import net.minecraft.registry.tag.TagKey
@@ -18,8 +19,11 @@ class WRTagGen(output: FabricDataOutput, registries: CompletableFuture<HolderLoo
         val DYE_TAGS = DyeColor.values().associateWith {
             TagKey.of(RegistryKeys.ITEM, Identifier("c", it.getName() + "_dyes"))
         }
-        val COLORED_BUNDLED_CABLES = TagKey.of(RegistryKeys.ITEM, id("colored_bundled_cables"))
-        val INSULATED_WIRES = TagKey.of(RegistryKeys.ITEM, id("insulated_wires"))
+        val COLORED_BUNDLED_CABLES: TagKey<Item> = TagKey.of(RegistryKeys.ITEM, id("colored_bundled_cables"))
+        val INSULATED_WIRES: TagKey<Item> = TagKey.of(RegistryKeys.ITEM, id("insulated_wires"))
+        val COLORED_STANDING_BUNDLED_CABLES: TagKey<Item> =
+            TagKey.of(RegistryKeys.ITEM, id("colored_standing_bundled_cables"))
+        val STANDING_INSULATED_WIRES: TagKey<Item> = TagKey.of(RegistryKeys.ITEM, id("standing_insulated_wires"))
     }
 
     override fun configure(arg: HolderLookup.Provider) {
@@ -27,14 +31,16 @@ class WRTagGen(output: FabricDataOutput, registries: CompletableFuture<HolderLoo
             getOrCreateTagBuilder(tag).add(DyeColorUtil.dye(color))
         }
         getOrCreateTagBuilder(COLORED_BUNDLED_CABLES).let {
-            for (color in DyeColor.values()) {
-                it.add(WRItems.BUNDLED_CABLES[color]!!)
-            }
+            for (color in DyeColor.values()) it.add(WRItems.BUNDLED_CABLES[color]!!)
         }
         getOrCreateTagBuilder(INSULATED_WIRES).let {
-            for (color in DyeColor.values()) {
-                it.add(WRItems.INSULATED_WIRES[color]!!)
-            }
+            for (color in DyeColor.values()) it.add(WRItems.INSULATED_WIRES[color]!!)
+        }
+        getOrCreateTagBuilder(COLORED_STANDING_BUNDLED_CABLES).let {
+            for (color in DyeColor.values()) it.add(WRItems.STANDING_BUNDLED_CABLES[color]!!)
+        }
+        getOrCreateTagBuilder(STANDING_INSULATED_WIRES).let {
+            for (color in DyeColor.values()) it.add(WRItems.STANDING_INSULATED_WIRES[color]!!)
         }
     }
 }
