d6c54ddedd32430cf4c5ced5d76eaa5354970cea
Kneelawk <kneelawk@gmail.com>
2023-09-10 16:30:12 -0700

    Improve Create wire connectability

--- changelogs/changelog-v0.7.6+1.20.1.md
+++ changelogs/changelog-v0.7.6+1.20.1.md
@@ -1,3 +1,4 @@
 Changes:
 
 * Fixed overlay-rendering matrix getting corrupted by other mods (looking at you Conjuring).
+* Improved wire connectivity with Create components like Nixie Tubes and gates.
--- src/main/kotlin/com/kneelawk/wiredredstone/datagen/WRBlockTagGen.kt
+++ src/main/kotlin/com/kneelawk/wiredredstone/datagen/WRBlockTagGen.kt
@@ -5,11 +5,17 @@ import net.fabricmc.fabric.api.datagen.v1.FabricDataOutput
 import net.fabricmc.fabric.api.datagen.v1.provider.FabricTagProvider.BlockTagProvider
 import net.minecraft.block.Blocks
 import net.minecraft.registry.HolderLookup
+import net.minecraft.util.Identifier
 import java.util.concurrent.CompletableFuture
 
 class WRBlockTagGen(output: FabricDataOutput, registries: CompletableFuture<HolderLookup.Provider>) :
     BlockTagProvider(output, registries) {
     override fun configure(arg: HolderLookup.Provider) {
-        getOrCreateTagBuilder(WRBlockTags.WIRE_GATE_CONNECTABLE).add(Blocks.REPEATER, Blocks.COMPARATOR)
+        getOrCreateTagBuilder(WRBlockTags.WIRE_GATE_CONNECTABLE)
+            .add(Blocks.REPEATER, Blocks.COMPARATOR)
+            .addOptional(Identifier("create", "pulse_repeater"))
+            .addOptional(Identifier("create", "pulse_extender"))
+            .addOptional(Identifier("create", "powered_latch"))
+            .addOptional(Identifier("create", "powered_toggle_latch"))
     }
 }
